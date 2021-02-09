b46290eb370069298f6e8a5ad83a87bbe4fc45df
mingalkar <mingalkar@gmail.com>
2021-02-09 18:19:46 +0530

    1st test case

---
+++ src/test/java/com/Resonantia/UITests/Test_Case_All.java
@@ -0,0 +1,14 @@
+package com.Resonantia.UITests;
+
+import org.testng.annotations.Test;
+
+public class Test_Case_All {
+	/**
+	 * this is for login page only
+	 */
+	@Test
+public void tc_01() {
+	
+System.out.println("Hi this is Mangesh");
+}
+}
